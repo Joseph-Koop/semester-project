ea25bec461b4703627ef88c4f2f57f94c580dee1
Joseph Koop <koopjoseph7@gmail.com>
2026-02-22 12:15:12 -0600

    Basic validation for class fields.

--- cmd/api/classes.go
+++ cmd/api/classes.go
@@ -4,13 +4,14 @@ import (
 	//   "encoding/json"
 	"fmt"
 	"net/http"
-	// import the data package which contains the definition for Comment
-	// "github.com/Joseph-Koop/json-project/internal/data"
+	// import the data package which contains the definition for Class
+	"github.com/Joseph-Koop/json-project/internal/data"
+	"github.com/Joseph-Koop/json-project/internal/validator"
 )
 
 func (a *applicationDependencies) postClassHandler(w http.ResponseWriter,
 	r *http.Request) {
-	// create a struct to hold a comment
+	// create a struct to hold a class
 	// we use struct tags[``] to make the names display in lowercase
 	var incomingData struct {
 		Studio  string `json:"studio"`
@@ -27,6 +28,27 @@ func (a *applicationDependencies) postClassHandler(w http.ResponseWriter,
 		return
 	}
 
+	// Copy the values from incomingData to a new Class struct
+	// At this point in our code the JSON is well-formed JSON so now
+	// we will validate it using the Validator which expects a Class
+	class := &data.Class {
+		Studio: incomingData.Studio,
+		Trainer: incomingData.Trainer,
+		Capacity_limit: incomingData.Capacity_limit,
+		Membership_tier: incomingData.Membership_tier,
+		Name: incomingData.Name,
+		Status: incomingData.Status,
+	}
+	// Initialize a Validator instance
+	v := validator.New()
+
+	// Do the validation
+	data.ValidateClass(v, class)
+	if !v.IsEmpty() {
+		a.failedValidationResponse(w, r, v.Errors)  // implemented later
+		return
+	}
+
 	// for now display the result
-	fmt.Fprintf(w, "+%v\n", incomingData)
+	fmt.Fprintf(w, "%+v\n", incomingData)
 }
--- internal/data/classes.go
+++ internal/data/classes.go
@@ -2,16 +2,35 @@ package data
 
 import (
   "time"
+  "github.com/Joseph-Koop/json-project/internal/validator"
 )
 
 // each name begins with uppercase so that they are exportable/public
 type Class struct {
-    ID int64
-    Studio  string 
-    Trainer  string 
-    Capacity_limit  int64 
-    Membership_tier  string
-    Name  string
-    Status  string
-    CreatedAt  time.Time
+    ID int64                  `json:"id"`
+    Studio  string            `json:"studio"`
+    Trainer  string           `json:"trainer"`
+    Capacity_limit  int64     `json:"capacity_limit"`
+    Membership_tier  string   `json:"membership_tier"`
+    Name  string              `json:"name"`
+    Status  string            `json:"status"`
+    CreatedAt  time.Time      `json:"-"`
+    //Version?
 } 
+
+// Create a function that performs the validation checks
+func ValidateClass(v *validator.Validator, class *Class) {
+    v.Check(class.Studio != "", "studio", "Must be provided.")                                  // check if the Studio field is empty
+    v.Check(class.Trainer != "", "trainer", "Must be provided.")                                // check if the Trainer field is empty
+    v.Check(class.Capacity_limit > 0, "capacity_limit", "Must be greater than 0.")              // check if the Capacity_limit field 0 or less
+    v.Check(class.Membership_tier != "", "membership_tier", "Must be provided.")                // check if the Membership_tier field is empty
+    v.Check(class.Name != "", "name", "Must be provided.")                                      // check if the Name field is empty
+    v.Check(class.Status != "", "status", "Must be provided.")                                  // check if the Status field is empty
+
+    v.Check(len(class.Studio) <= 50, "studio", "Must not be more than 50 bytes long.")                      // check if the Studio field is bigger than 50 characters
+    v.Check(len(class.Trainer) <= 50, "trainer", "Must not be more than 50 bytes long.")                    // check if the Trainer field is bigger than 50 characters
+    v.Check(class.Capacity_limit <= 100, "capacity_limit", "Must be less than or equal to 100.")            // check if the Capacity_limit field is bigger than 100
+    v.Check(len(class.Membership_tier) <= 50, "membership_tier", "Must not be more than 50 bytes long.")    // check if the Membership_tier field is bigger than 50 characters
+    v.Check(len(class.Name) <= 50, "name", "Must not be more than 50 bytes long.")                          // check if the Name field is bigger than 50 characters
+    v.Check(len(class.Status) <= 50, "status", "Must not be more than 50 bytes long.")                      // check if the Status field is bigger than 50 characters
+}
